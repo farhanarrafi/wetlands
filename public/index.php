d5e7745335458c5d804013c65fbbc31dc1715972
Farhan Ar Rafi <farhan.rafi@bjitgroup.com>
2017-10-02 15:18:18 +0600

    Wetlands_master: Updated pages
    - Added pages for New Data and About
    - Added routes and other configs
    - Removed vue.js
    
    Refs: N/A

--- public/index.php
+++ public/index.php
@@ -1,6 +1,22 @@
 <?php
-require '../vendor/autoload.php';
-require_once '../app/views/header.php';
-require_once '../app/views/content.php';
-require_once '../app/views/footer.php';
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// Instantiate the app
+$settings = require __DIR__ . '/../app/settings/settings.php';
+$app = new \Slim\App($settings);
+
+// Set up dependencies
+require __DIR__ . '/../app/dependencies/dependencies.php';
+
+// Register middleware
+require __DIR__ . '/../app/middleware/middleware.php';
+
+// Register routes
+require __DIR__ . '/../app/routes/routes.php';
+
+// Run the app
+$app->run();
 ?>
